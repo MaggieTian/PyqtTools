3a0bfdc784c30e814b302328c46adaf983aa25f9
maggie <qi.tian@zongmutech.com>
2017-12-20 19:15:42 +0800

    fix bug:can not read large file and encoding problem

--- Pyqt_Log_Aanalyser/util.py
+++ Pyqt_Log_Aanalyser/util.py
@@ -3,7 +3,11 @@
 # @Time    : 2017/12/11
 # @Author  : tianqi
 # @File    : util.py
-from PyQt5.QtWidgets import QDesktopWidget, QProgressBar, QWidget
+
+from PyQt5.QtWidgets import QDesktopWidget, QProgressBar, QWidget, QMessageBox
+
+
+
 class Util():
 
     def __init__(self):
@@ -27,7 +31,6 @@ class Util():
     def start_analysis(self,log_path,find_data):
 
         try:
-            log_file = open(log_path,"r")
             result_info = {}
             result = {}
             # 初始化存放每个查找信息的数据结构
@@ -39,29 +42,36 @@ class Util():
                 result_info = {}  # 清空当前循环存储的内容
 
             print(result)
+            linenum = 0  # 记录文件行数
 
-            lines = log_file.readlines()
-            for index, line in enumerate(lines):
-                for k, v in result.items():
-                    for text, info in v.items():
-                        if line.find(text) > -1:
-                            info['cnt'] += 1
-                            info['line'].append(index)
-            print(result)
-            return True,result
+            '''with语句打开和关闭文件，包括抛出一个内部块异常。for line in f文件对象f视为一个迭代器，
+             会自动的采用缓冲IO和内存管理，所以你不必担心大文件。'''
+
+            with open(log_path,"rb") as f:     # 以二进制读取文件内容方式读取文件内容，转行成字符串进行操作，可解决编码问题
+                for line in f:
+                    linenum += 1
+                    for k, v in result.items():
+                        for text, info in v.items():
+                            if str(line).find(text) > -1:
+                                info['cnt'] += 1
+                                info['line'].append(linenum)
+
+                print (linenum)
+                return True,result
 
         # 程序执行出现异常
-        except Exception:
-            return False
+        except Exception :
+            raise  Exception
+            # return False
 
 
     def write_to_excel(self):
         pass
 
 if __name__ == '__main__':
-    log_path = r"C:\Users\qtian\Desktop\1212左右转向进全景.log"
-    data = {'1': ['car_signals_handler/55, old: 0x0, new: 0x2', 'fb_layer_mgr_set_group_prio: set avm3d_ui prio to 101'], '2': ['car_live_info_mgr_set_signals/165, car signal 0x2', 'fb_layer_mgr_set_group_prio: set avm3d_ui prio to 101']}
-    Util().start_analysis(log_path,"ss",data)
+    log_path = r"C:\Users\qtian\Desktop\1219.log"
+    data = {'1': [' Kernel image', '333']}
+    print (Util().start_analysis(log_path,data))
 
 
 
