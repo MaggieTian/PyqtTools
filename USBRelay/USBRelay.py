ea60685e69bb27c85fd52703877e4da3a10c5dd1
maggie <qi.tian@zongmutech.com>
2018-01-24 16:36:29 +0800

    add logger to record run or error msg

--- USBRelay/USBRelay.py
+++ USBRelay/USBRelay.py
@@ -9,6 +9,15 @@ import serial
 import serial.tools.list_ports
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("usbrelay.log")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 class MainWindow(QMainWindow,Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -16,18 +25,19 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.start.clicked.connect(self.start_run)  # 开始控制继电器
         self.stop_button.clicked.connect(self.stop)
         self.stop_button.setEnabled(False)
+
+        # 设置延时和循环次数的默认值
         self.lineEdit_wait.setText(str(10))
         self.lineEdit_loop.setText(str(10000))
         self.lineEdit_off_on.setText(str(6))
         self.serial = None
-        self.index = 1                              # 记录combox的所有item
 
     def start_run(self):
 
         wait_time = self.lineEdit_wait.text()
         loop = self.lineEdit_loop.text()
         com = self.comboBox.currentText().split(" ")[0]
-        print(com)
+        logger.info("目前连接的串口号是{0}".format(com))
         off_to_on_time = self.lineEdit_off_on.text()
         self.stop_button.setEnabled(True)
 
@@ -58,13 +68,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
                 self.thread.quit()    # 运行结束后结束线程
             except Exception as e:
                 QMessageBox.information(self, "Warning!", "发生错误!\n错误信息："+str(e))
-                logging.error("转换输入值时发送错误\n"+str(e))
+                logger.error("转换输入值时发送错误\n"+str(e))
 
-    # 获取接入的所有串口号
-    def get_port_list(self):
-        port_list = list(serial.tools.list_ports.comports())
-        for port in port_list:
-            yield str(port)
 
     # lcd组件接收到信号后显示数字
     def display(self, times):
@@ -76,16 +81,16 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             # 按下停止键就结束线程，不进行下一步的循环
             self.thread.terminate()
             self.thread.wait(10)   # 确保线程终止
-            print("结束线程")
+            logger.info("结束线程")
             if self.serial.isOpen(): # 检测串口是否关闭，若没关闭则关闭串口
                 self.serial.close()
             self.start.setEnabled(True)
             self.stop_button.setEnabled(False)
         except Exception as e:
-            logging.error("停止线程失败，错误信息：{0}".format(str(e)))
+            logger.error("停止线程失败，错误信息：{0}".format(str(e)))
 
     def compelted(self, msg):
-        print(msg)
+        logger.info("循环次数"+msg)
         QMessageBox.information(self,"提示","已完成{0}次上下电".format(msg.split(" ")[1]))
         self.start.setEnabled(True)
 
@@ -114,7 +119,7 @@ class RunThread(QThread):
             self._serial.close()                                    # close port
             self.done_signal.emit("Done {0}".format(str(self._loop)))   # 发出完成信号
         except Exception as e:
-            logging.error("向继电器发送指令过程中出现了异常:{0}".format(e))
+            logger.error("向继电器发送指令过程中出现了异常:{0}".format(e))
             raise e  # 抛出异常
 
 
--- USBRelay/custom_comboBox.py
+++ USBRelay/custom_comboBox.py
@@ -5,6 +5,7 @@
 # @File    : custom_comboBox.py
 import serial
 import serial.tools.list_ports
+import logging
 from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtCore import pyqtSignal
 
@@ -15,22 +16,26 @@ class CustomComboBox(QComboBox):
     def __init__(self, parent = None):
         super(CustomComboBox,self).__init__(parent)
 
-
     # 重写showPopup函数
     def showPopup(self):
-        # self.popupAboutToBeShown.emit()
+        # 先清空原有的选项
         self.clear()
         self.insertItem(0, "请选择继电器串口号")
         index = 1
-        portlist = self.get_port_list()
+        # 获取接入的所有串口信息，插入combobox的选项中
+        portlist = self.get_port_list(self)
         if portlist is not None:
             for i in portlist:
                 self.insertItem(index, i)
                 index += 1
-        QComboBox.showPopup(self)
+        QComboBox.showPopup(self)   # 弹出选项框
 
-# 获取接入的所有串口号
+    @staticmethod
+    # 获取接入的所有串口号
     def get_port_list(self):
-        port_list = list(serial.tools.list_ports.comports())
-        for port in port_list:
-            yield str(port)
+        try:
+            port_list = list(serial.tools.list_ports.comports())
+            for port in port_list:
+                yield str(port)
+        except Exception as e:
+            logging.error("获取接入的所有串口设备出错！\n错误信息："+str(e))
